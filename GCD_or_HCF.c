1fb1f318f83075f0a7f0b8b9de35943bb06777c8
Asrith4074 <119718386+Asrith4074@users.noreply.github.com>
2023-09-18 21:25:23 +0530

    #include<stdio.h> int main() {     int i,a,b,g;     scanf("%d %d",&a,&b);     for(i=1;i<=a && i<=b;i++)     {         if(a%i==0 && b%i==0)         g=i;     }     printf("%d",g); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -2,11 +2,11 @@
 int main()
 {
     int i,a,b,g;
-    scanf("%d%d",&a,&b);
+    scanf("%d %d",&a,&b);
     for(i=1;i<=a && i<=b;i++)
     {
         if(a%i==0 && b%i==0)
-          g=i;
+        g=i;
     }
     printf("%d",g);
 }
